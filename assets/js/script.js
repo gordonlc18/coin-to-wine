5976a4c180b2c76283a92b44e51b920d9790c089
Ernest Paul <ernestpaul88@gmail.com>
2022-02-11 11:55:32 -0500

    refined the coin gecko fetch and dynamically displayed price

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,32 @@
+$(function () {
+  $("#rando-btn").on("click", function () {
+    getBitcoinPrice();
+  });
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://coingecko.p.rapidapi.com/simple/price?ids=bitcoin&vs_currencies=usd",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "coingecko.p.rapidapi.com",
-		"x-rapidapi-key": "b27e1aaebdmsh5e18b202a907fe1p1cc241jsn89e10075c08e"
-	}
-};
+  function getBitcoinPrice() {
+    const settings = {
+      async: true,
+      crossDomain: true,
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "coingecko.p.rapidapi.com",
+        "x-rapidapi-key": "b27e1aaebdmsh5e18b202a907fe1p1cc241jsn89e10075c08e",
+      },
+    };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+    var apiURL =
+      "https://coingecko.p.rapidapi.com/simple/price?ids=bitcoin&vs_currencies=usd";
+
+    fetch(apiURL, settings)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data.bitcoin.usd);
+        $("#bitcoin-Price").text(data.bitcoin.usd);
+      });
+  }
 });
-
 
 // const settings = {
 // 	"async": true,
