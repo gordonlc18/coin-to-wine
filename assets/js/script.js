15f2962f4ce3e7c209314df8170c569b52aea4ef
bekkahhuss <bekkahhuss@gmail.com>
2022-02-10 20:35:57 -0500

    working on adding coingecko and google scraper API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-$(function () {});
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://coingecko.p.rapidapi.com/simple/price?ids=bitcoin&vs_currencies=usd",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coingecko.p.rapidapi.com",
+		"x-rapidapi-key": "b27e1aaebdmsh5e18b202a907fe1p1cc241jsn89e10075c08e"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://google-shopping-scraper.p.rapidapi.com/scrape",
+// 	"method": "POST",
+// 	"headers": {
+// 		"content-type": "application/json",
+// 		"x-rapidapi-host": "google-shopping-scraper.p.rapidapi.com",
+// 		"x-rapidapi-key": "b27e1aaebdmsh5e18b202a907fe1p1cc241jsn89e10075c08e"
+// 	},
+// 	"processData": false,
+// 	"data": {
+// 		"query": "iphone case",
+// 		"country": "us"
+// 	}
+// };
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
